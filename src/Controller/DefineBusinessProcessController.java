3a6a8ceaf5c53effe3638b6195d37c73ef265876
RaviAlagiya <Ravi@localhost>
2016-10-29 12:01:57 -0500

    second

---
+++ src/Controller/DefineBusinessProcessController.java
@@ -0,0 +1,50 @@
+package Controller;
+
+import java.util.List;
+
+import BusinessObjects.BusinessProcess;
+import BusinessObjects.Repository;
+import BusinessObjects.Step;
+import Commands.AddComponent;
+import Commands.GetComponent;
+
+public class DefineBusinessProcessController {
+	Repository repository;
+	
+	public DefineBusinessProcessController(Repository repository){
+		this.repository = repository;
+	}
+	
+	public void AddBusinessProcess(BusinessProcess businessprocess){
+		
+		AddComponent add = new AddComponent(repository, businessprocess);
+		add.execute();
+	}
+	
+    public void AddStep(Step step,int position){
+		
+    	GetComponent get = new GetComponent(repository);
+    	get.execute();
+    	
+    	BusinessProcess parent = ((List<BusinessProcess>)get.getResult()).get(position);
+    	
+		AddComponent add = new AddComponent(repository, parent);
+		add.execute();
+	}
+    
+    public void AddAction(Step step,int position1, int position2){
+		
+    	GetComponent get = new GetComponent(repository);
+    	get.execute();
+    	
+    	BusinessProcess bp = ((List<BusinessProcess>)get.getResult()).get(position1);
+    	
+    	get = new GetComponent(bp);
+    	get.execute();
+    	
+    	Step parent = ((List<Step>)get.getResult()).get(position2);
+    	
+		AddComponent add = new AddComponent(repository, parent);
+		add.execute();
+	}
+}
