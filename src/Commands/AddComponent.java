b27c91e3e9e662bdcf93c77fd1c9b167b1d41bc2
JarvisZhangUTA <zhangjw.uta@gmail.com>
2016-10-29 20:19:09 -0500

    addcomponent

--- src/Commands/AddComponent.java
+++ src/Commands/AddComponent.java
@@ -9,27 +9,31 @@ import BusinessObjects.Step;
 public class AddComponent extends ListCommand {
 	Object parent;
 	RequirementComponent child;
+	int index;
 	
-	public AddComponent(Object parent,RequirementComponent child) {
+	public AddComponent(Object parent,RequirementComponent child,int index) {
 		this.parent=parent;
 		this.child=child;
+		this.index = index;
 	}
-	
 	
 	public boolean execute()
 	{
+		
 		if(parent instanceof Repository )
 		{
-			 return ((Repository) parent).getBusinessProcessList().add((BusinessProcess) child);
+			((Repository) parent).getBusinessProcessList().add(index,(BusinessProcess) child);
+			return true;
 		}
 		else if(parent instanceof BusinessProcess)
 		{
-			return ((BusinessProcess)parent).getStepsList().add((Step) child);
-			 //((BusinessProcess)parent).ggetadd((Step) child);
+			((BusinessProcess)parent).getStepsList().add(index ,(Step) child);
+			return true;
 		}
 		else if(parent instanceof Step)
 		{
-			return ((Step)parent).getActionsList().add((Action) child);
+			((Step)parent).getActionsList().add(index ,(Action) child);
+			return true;
 		}
 			
 		return false;
